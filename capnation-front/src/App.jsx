09d332b10d615e075ed53a6efdefc92899f9d065
Brayan Jaraba <brayanjaraba2006@gmail.com>
2025-11-23 20:33:14 -0500

    feat: add form to add new caps

--- capnation-front/src/App.jsx
+++ capnation-front/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { Routes, Route } from "react-router";
 import { TopBar } from "./components/TopBar.jsx";
 import { Footer } from "./components/Footer.jsx";
 import { Home } from "./pages/Home.jsx";
+import { SaveCap } from "./pages/SaveCap.jsx";
 function App() {
   return (
     <>
       <TopBar />
       <Routes>
+        <Route path="/save-cap" element={<SaveCap />}></Route>
         <Route path="/" element={<Home />}></Route>
       </Routes>
       <Footer />
